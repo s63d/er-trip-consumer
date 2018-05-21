68eb0d266d7ea47c0cd6c3e63c150e71c2ebc0b6
Marvin <marvin.zwolsman@gmail.com>
2018-05-21 11:57:10 +0200

    Run on port 8083

--- src/main/kotlin/com/s63d/ertripconsumer/ErTripServiceApplication.kt
+++ src/main/kotlin/com/s63d/ertripconsumer/ErTripServiceApplication.kt
@@ -9,5 +9,5 @@ import org.springframework.cloud.openfeign.EnableFeignClients
 class ErTripConsumerApplication
 
 fun main(args: Array<String>) {
-    runApplication<ErTripServiceApplication>(*args)
+    runApplication<ErTripConsumerApplication>(*args)
 }
