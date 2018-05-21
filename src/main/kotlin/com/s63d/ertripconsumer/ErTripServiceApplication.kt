3df6fe0850713e6a0859504bf86192876dc2ebba
Marvin <marvin.zwolsman@gmail.com>
2018-05-21 11:58:26 +0200

    Strip it more!

--- src/main/kotlin/com/s63d/ertripconsumer/ErTripServiceApplication.kt
+++ src/main/kotlin/com/s63d/ertripconsumer/ErTripServiceApplication.kt
@@ -2,10 +2,8 @@ package com.s63d.ertripconsumer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-@EnableFeignClients
 class ErTripConsumerApplication
 
 fun main(args: Array<String>) {
