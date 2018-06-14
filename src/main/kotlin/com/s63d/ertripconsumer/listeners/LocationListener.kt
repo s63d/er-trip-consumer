222ed4351665a465677945ced6f8b7db5e71892b
Marvin <marvin.zwolsman@gmail.com>
2018-06-14 13:41:35 +0200

    Updated consumer to use new binding

--- src/main/kotlin/com/s63d/ertripconsumer/listeners/LocationListener.kt
+++ src/main/kotlin/com/s63d/ertripconsumer/listeners/LocationListener.kt
@@ -2,17 +2,20 @@ package com.s63d.ertripconsumer.listeners
 
 import com.s63d.ertripconsumer.domain.Location
 import com.s63d.ertripconsumer.repositories.LocationRepository
+import org.springframework.amqp.rabbit.annotation.Exchange
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.QueueBinding
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 
 @Component
-class ProftaakListener(private val locationRepository: LocationRepository) {
+class LocationListener(private val locationRepository: LocationRepository) {
 
     data class LocationMessage(val tripId: Long, val lat: Double, val lon: Double) {
         private constructor() : this(0,0.0,0.0)
     }
 
-    @RabbitListener(queues = ["proftaak"])
+    @RabbitListener(bindings = [(QueueBinding(value = Queue("INTERN_AT_LOCATION"), key = ["location"], exchange = Exchange("AT")))])
     fun onLocationMessage(message: LocationMessage) {
         locationRepository.save(Location(message.lat, message.lon, message.tripId))
     }
